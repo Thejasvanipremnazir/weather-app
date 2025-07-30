82f3ed7630a18415f3ebc376929a44d67a050a5e
Thejasvanipremnazir <thejasvanipremnazir@gmail.com>
2025-07-30 23:24:05 +0530

    Create SearchBar.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+function SearchBar({ city, setCity, handleSearch }) {
+  return (
+    <div>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city name"
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
